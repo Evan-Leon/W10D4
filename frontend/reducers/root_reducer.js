e7b9d6c9eef7f115cd249c58e7b59e066c8ffc88
Evan-Leon <evan.leon.j@gmail.com>
2021-04-29 13:59:52 -0400

    Added actions and reducers

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,20 @@
+import { combineReducers } from "redux";
+import todosReducer from "./todos_reducer.js";
+
+const rootReducer = combineReducers({
+    todos: todosReducer,
+})
+
+
+
+
+
+export default rootReducer;
+
+
+
+// const rootReducer = (state, action) => {
+//     return {
+//         todos: todosReducer(state.todos, action),
+//     }
+// }
